872e0622deef3d68aee9bc84e6672baf11b02819
pratikpp555 <31356566+pratikpp555@users.noreply.github.com>
2019-07-28 20:51:31 +0530

    Update Sock_Merchant.py

--- HackerRank-Sock Merchant/Sock_Merchant.py
+++ HackerRank-Sock Merchant/Sock_Merchant.py
@@ -9,11 +9,12 @@ import sys
 # Complete the sockMerchant function below.
 def sockMerchant(n, ar):
     count = 0
-    ar.sort()
-    ar.append('#')
+    br=ar.copy()
+    br.sort()
+    br.append('#')
     i = 0
     while i<n:
-        if ar[i]==ar[i+1]:
+        if br[i]==br[i+1]:
             count = count+1
             i+=2
         else:
